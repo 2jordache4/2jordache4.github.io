fbe5c0d1aacc70121ec1e672c7562192852161f2
2jordache4 <cousineau.jordan24@gmail.com>
2026-03-08 13:34:37 -0400

    Added diplomas

--- src/components/education.jsx
+++ src/components/education.jsx
@@ -4,11 +4,11 @@ const education = {
   "Boston University": {degree: "M.S.", major: "Robotics & Autonomous Systems",
     description: "I attended BU from August 2024 to January 2026. \
     I took notable courses such as: Robot Motion planning, Soft Robotics, Embedded Systems, Image & Video Processing, and Advanced Dynamics.", 
-    image: "./assets/ascii-art.png" },
+    image: "./assets/diploma_ms.png" },
   "Pacific University": {degree: "B.S.", major: "Mathematics",
     description: "I attended PU from 2020 to 2024. I was an active member of the mens tennis team and Women in Computer Science club. \
     I took notable course such as: Linear Algebra, Probability and Statistics, Calculus 3, Data Structures, Databases, Theoretical Computer Science, and Human Computer Interaction.", 
-    image: "./assets/ascii-art.png" },
+    image: "./assets/diploma_bs.png" },
 }
 
 function Education() {
